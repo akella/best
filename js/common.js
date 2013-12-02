cb175d9fa73e72dd2ea7f6ac200815a8f08613c7
akella <akella@Yuris-MacBook-Pro.local>
2013-12-03 00:07:58 +0200

    slider

--- js/common.js
+++ js/common.js
@@ -7,4 +7,17 @@ $(document).ready(function() {
 		$(this).children('a').addClass('is-active');
 		return false;
 	});
+
+	$( "#slider-range" ).slider({
+      range: true,
+      min: 0,
+      max: 30000,
+      values: [ 1000, 30000 ],
+      slide: function( event, ui ) {
+      	$('.search__pricevalue1').text(ui.values[ 0 ]);
+      	$('.search__pricevalue2').text(ui.values[ 1 ]);
+      }
+    });
+    $('.search__pricevalue1').text($( "#slider-range" ).slider( "values", 0 ));
+    $('.search__pricevalue2').text($( "#slider-range" ).slider( "values", 1 ));
 });
