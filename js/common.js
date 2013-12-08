4d84692b4f275bb2d339c1139382434692823491
akella <akella@Yuris-MacBook-Pro.local>
2013-12-08 18:40:26 +0200

    all the ranges working

--- js/common.js
+++ js/common.js
@@ -15,12 +15,58 @@ $(document).ready(function() {
         max: 30000,
         values: [ 1000, 30000 ],
         slide: function( event, ui ) {
-         $('.search__pricevalue1').text(ui.values[ 0 ]);
-         $('.search__pricevalue2').text(ui.values[ 1 ]);
+         $('#search__pricevalue1').text(ui.values[ 0 ]);
+         $('#search__pricevalue2').text(ui.values[ 1 ]);
         }
       });
-      $('.search__pricevalue1').text($( "#slider-range" ).slider( "values", 0 ));
-      $('.search__pricevalue2').text($( "#slider-range" ).slider( "values", 1 ));
+      $('#search__pricevalue1').text($( "#slider-range" ).slider( "values", 0 ));
+      $('#search__pricevalue2').text($( "#slider-range" ).slider( "values", 1 ));
+    }
+
+    if($("#slider-range1").length > 0){
+      $( "#slider-range1" ).slider({
+        range: true,
+        min: 0,
+        max: 30000,
+        values: [ 1000, 30000 ],
+        slide: function( event, ui ) {
+         $('#search__rangevalue1').text(ui.values[ 0 ]);
+         $('#search__rangevalue2').text(ui.values[ 1 ]);
+        }
+      });
+      $('#search__rangevalue1').text($( "#slider-range1" ).slider( "values", 0 ));
+      $('#search__rangevalue2').text($( "#slider-range1" ).slider( "values", 1 ));
+    }
+
+
+    if($("#slider-range2").length > 0){
+      $( "#slider-range2" ).slider({
+        range: true,
+        min: 0,
+        max: 30000,
+        values: [ 1000, 30000 ],
+        slide: function( event, ui ) {
+         $('#search__range2value1').text(ui.values[ 0 ]);
+         $('#search__range2value2').text(ui.values[ 1 ]);
+        }
+      });
+      $('#search__range2value1').text($( "#slider-range2" ).slider( "values", 0 ));
+      $('#search__range2value2').text($( "#slider-range2" ).slider( "values", 1 ));
+    }
+
+    if($("#slider-range3").length > 0){
+      $( "#slider-range3" ).slider({
+        range: true,
+        min: 0,
+        max: 30000,
+        values: [ 1000, 30000 ],
+        slide: function( event, ui ) {
+         $('#search__range3value1').text(ui.values[ 0 ]);
+         $('#search__range3value2').text(ui.values[ 1 ]);
+        }
+      });
+      $('#search__range3value1').text($( "#slider-range3" ).slider( "values", 0 ));
+      $('#search__range3value2').text($( "#slider-range3" ).slider( "values", 1 ));
     }
 	
 
