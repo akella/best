bf74a06c57a4b652ab1e06c361d9637511f703d9
akella <akella@Yuris-MacBook-Pro.local>
2013-12-10 00:38:45 +0200

    fixed to top menu, some hovers, scripts

--- js/common.js
+++ js/common.js
@@ -80,11 +80,40 @@ $(document).ready(function() {
 
     //gallery
     
-
+    if($(".scroll-pane").length > 0){
       $('.scroll-pane').jScrollPane({
         horizontalDragMaxWidth: 50
       });
+    }
 
+
+
+
+
+  var stickyPanelSettings = {
+        // Use this to set the top margin of the detached panel.
+        topPadding: 10,
+
+        // This class is applied when the panel detaches.
+        afterDetachCSSClass: "",
+
+        // When set to true the space where the panel was is kept open.
+        savePanelSpace: true,
+
+        // Event fires when panel is detached
+        // function(detachedPanel, panelSpacer){....}
+        onDetached: null,
+
+        // Event fires when panel is reattached
+        // function(detachedPanel){....}
+        onReAttached: null,
+
+        // Set this using any valid jquery selector to 
+        // set the parent of the sticky panel.
+        // If set to null then the window object will be used.
+        parentSelector: null
+    };
+    $(".tabs-menu").stickyPanel(stickyPanelSettings);
 
 
 });
